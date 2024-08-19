a4cab007ca18eac26bd896f293230e61f25f727c
Werllen Castro <hi@werls.xyz>
2024-08-19 20:06:12 -0300

    Refactor main.py to use settings dictionary for Halftone instantiation

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import argparse
+import json
 from math import radians
 from drawBot import newDrawing, endDrawing
 from halftone import Halftone
+
+def str2bool(v):
+    return v.lower() in ('true', '1', 'yes')
 
 def main():
     parser = argparse.ArgumentParser(
@@ -13,19 +17,19 @@ def main():
                         help='The contrast of the halftone dots. Defaults to 1.')
     parser.add_argument('--angle', type=float, default=45,
                         help='The angle of rotation for the halftone dots in degrees. Defaults to 45.')
-    parser.add_argument('--verbose', type=lambda x: (str(x).lower() == 'true'), default=True,
+    parser.add_argument('--verbose', type=str2bool, nargs='?', const=True, default=True,
                         help='Enable verbose output. Defaults to True.')
-    parser.add_argument('--save', type=lambda x: (str(x).lower() == 'true'), default=True,
+    parser.add_argument('--save', type=str2bool, nargs='?', const=True, default=True,
                         help='Save the image as a PDF file. Defaults to True.')
-    parser.add_argument('--honeycomb', type=lambda x: (str(x).lower() == 'true'), default=True,
+    parser.add_argument('--honeycomb', type=str2bool, nargs='?', const=True, default=True,
                         help='Use a honeycomb grid. Defaults to True.')
-    parser.add_argument('--reescale_image', type=lambda x: (str(x).lower() == 'true'), default=False,
+    parser.add_argument('--reescale_image', type=str2bool, nargs='?', const=True, default=False,
                         help='Rescale the image to fit the page. Defaults to False.')
-    parser.add_argument('--inverted', type=lambda x: (str(x).lower() == 'true'), default=False,
+    parser.add_argument('--inverse', type=str2bool, nargs='?', const=True, default=False,
                         help='Invert the color. Defaults to False.')
+    parser.add_argument('--settings', type=str, help='A JSON string or file path of settings for the halftone effect.')
 
     args = parser.parse_args()
-
     path = args.path
 
     settings = {
@@ -35,16 +39,24 @@ def main():
         'verbose': args.verbose,
         'save': args.save,
         'use_honeycomb_grid': args.honeycomb,
-        'reescale_image': args.reescale_image
+        'reescale_image': args.reescale_image,
+        'inverse': args.inverse
     }
+
+    if args.settings:
+        try:
+            with open(args.settings, 'r') as file:
+                file_settings = json.load(file)
+            settings.update(file_settings)
+        except FileNotFoundError:
+            settings.update(json.loads(args.settings))
 
     newDrawing()
 
-    # Instantiate and run the Reticulate class
+    # Instantiate and run the Halftone class
     halftone = Halftone(path, settings)
 
     endDrawing()
-
 
 if __name__ == '__main__':
     main()
