b9c207cebde50b18fb1e083576aec0f1d71cb7be
Werllen Castro <hi@werls.xyz>
2024-08-30 13:10:20 -0300

    Refactor argparse configuration for halftone effect

--- main.py
+++ main.py
@@ -8,36 +8,39 @@ def str2bool(v):
     return v.lower() in ('true', '1', 'yes')
 
 def main():
-    parser = argparse.ArgumentParser(
-        description='Create a halftone effect using the DrawBot library.')
+    parser = argparse.ArgumentParser(description='Create a halftone effect using the DrawBot library.')
+    
     parser.add_argument('positional_path', nargs='?', help='The path to the image file.')
-    parser.add_argument('--path', type=str, help='The path to the image file.')
-    parser.add_argument('--resolution', type=int, default=100,
+    parser.add_argument('--path', type=str, nargs='?', help='The path to the image file.')
+    parser.add_argument('--resolution', '-r', type=int, nargs='?', default=100,
                         help='The resolution of the halftone grid. Defaults to 100.')
-    parser.add_argument('--contrast', type=float, default=1,
+    parser.add_argument('--contrast', '-c', type=float, nargs='?', default=1,
                         help='The contrast of the halftone dots. Defaults to 1.')
-    parser.add_argument('--dot_min_size', type=float, default=0,
+    parser.add_argument('--dot-min-size', type=float, nargs='?', default=0,
                         help='The minimum size of the halftone dots. Defaults to 0')
-    parser.add_argument('--angle', type=float, default=45,
+    parser.add_argument('--angle', type=float, nargs='?', default=45,
                         help='The angle of rotation for the halftone dots in degrees. Defaults to 45.')
     parser.add_argument('--verbose', type=str2bool, nargs='?', const=True, default=True,
                         help='Enable verbose output. Defaults to True.')
-    parser.add_argument('--save', type=str2bool, nargs='?', const=True, default=True,
-                        help='Save the image as a PDF file. Defaults to True.')
+    # parser.add_argument('--save', type=str2bool, nargs='?', const=True, default=True,
+    #                     help='Save the image as a file. Defaults to True.')
     parser.add_argument('--honeycomb', type=str2bool, nargs='?', const=True, default=True,
                         help='Use a honeycomb grid. Defaults to True.')
-    parser.add_argument('--reescale_image', type=str2bool, nargs='?', const=True, default=False,
+    parser.add_argument('--reescale', type=str2bool, nargs='?', const=True, default=False,
                         help='Rescale the image to fit the page. Defaults to False.')
     parser.add_argument('--inverse', type=str2bool, nargs='?', const=True, default=False,
                         help='Invert the color. Defaults to False.')
-    parser.add_argument('--preset', type=str, 
+    parser.add_argument('--preset', '-p', type=str, nargs='?',
                         help='A JSON string or file path of settings for the halftone effect.')
-    parser.add_argument('--color', type=str, 
+    parser.add_argument('--color', type=str, nargs='?',
                         help='The color of the halftone dots in RGBA or CMYK format.', default='0,0,0,255')
-    parser.add_argument('--color_mode', type=str, 
+    parser.add_argument('--color-mode', type=str, nargs='?',
                         help='The color mode of the halftone dots. (rgba or cmyk)', default='rgba')
-    parser.add_argument('--save-format', type=str, 
+    parser.add_argument('--output-format', type=str, nargs='?',
                         help='The format of the saved image. (pdf, png, jpg)', default='pdf')
+    parser.add_argument('--output-dir', "-o", type=str, nargs='?', help='The directory to save the image file.')
+    parser.add_argument('--keep-original-name', '-k', type=str2bool, nargs='?', const=True, default=False,
+                        help='Keep the original name of the image file. Defaults to False.')
 
     args = parser.parse_args()
     path = args.path or args.positional_path
@@ -47,17 +50,22 @@ def main():
         'contrast': args.contrast,
         'angle': radians(args.angle),
         'verbose': args.verbose,
-        'save': args.save,
+        # 'save': args.save, # CLI use should always save the image
+        'save': True,
         'use_honeycomb_grid': args.honeycomb,
-        'reescale_image': args.reescale_image,
+        'reescale': args.reescale,
         'inverse': args.inverse,
         'color': [float(c) for c in args.color.split(',')],
         'color_mode': args.color_mode,
         'dot_min_size': args.dot_min_size,
-        'save_format': args.save_format
+        'output_format': args.output_format,
+        'output_dir': args.output_dir,
+        'keep_original_name': args.keep_original_name
     }
 
     if args.preset:
+        # incluir uma checagem para verificar se o path é válido
+
         try:
             with open(args.preset, 'r') as file:
                 preset = json.load(file)
